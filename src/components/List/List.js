40f5bb0df7771d18d9a48aaf5a88e76ae8e429c4
Dagmara Tomasik <Tomasik.Dagmara@outlook.com>
2019-09-05 15:21:32 +0200

    Fixed column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,23 +3,35 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  }
+
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
     image: PropTypes.string,
-    column: PropTypes.array,
+    description: PropTypes.node,
+    columns: PropTypes.array,
   }
+
+  static defaultProps = {
+    description: settings.defaultListDescription,
+  }
+
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} image={this.props.image} />
+        <div className={styles.description}>{ReactHtmlParser(this.props.description)}</div>
         <div className={styles.columns}>
-          <Column title={'Clean the house'} />
-          <Column title={'Cooking'} />
-          <Column title={'Walk the dog'} />
+        {this.state.columns.map(({key, ...columnProps}) => (
+          <Column key={key} {...columnProps} />
+        ))}
         </div>
       </section>
     )
